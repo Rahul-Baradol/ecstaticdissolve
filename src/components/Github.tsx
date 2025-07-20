32f0b02105d3606209a1480074e543c474c3cab4
Rahul Baradol <rahul.baradol.14@gmail.com>
2025-07-20 23:09:46 +0530

    hiding scrollbar

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -7,7 +7,7 @@ export default function GitHubFloatButton() {
       href="https://github.com/Rahul-Baradol/ecstaticdissolve"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 right-4 z-50 bg-black text-white rounded-full w-12 h-12 flex items-center justify-center shadow-lg hover:bg-gray-800 transition"
+      className="fixed bottom-6 right-6 z-50 bg-black text-white rounded-full w-12 h-12 flex items-center justify-center shadow-lg hover:bg-gray-800 transition"
     >
       <FaGithub size={24} />
     </Link>
